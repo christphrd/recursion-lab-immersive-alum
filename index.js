2f5f88ec484e3c24aeb669c053ca8dca2e019207
christphrd <diep.christopher@gmail.com>
2018-08-02 15:39:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0])
+
+  if (string.length > 1) {
+    printString(string.substring(1))
+  } else {
+    return true
+  }
+}
+
+function reverseString(str) {
+  if (str.length > 1) {
+    return str.substring(str.length - 1) + reverseString(str.substring(0,str.length - 1))
+  } else {
+    return str
+  }
+}
+
+function isPalindrome(str) {
+  let strArr = str.split("")
+  if (str.length > 2 && strArr[0] === strArr[strArr.length - 1]) {
+    strArr.shift()
+    strArr.pop()
+    return isPalindrome(strArr.join(""))
+  } else {
+    return strArr[0] === strArr[strArr.length - 1]
+  }
+}
+
+function addUpTo(arr, index) {
+  if (index !== 0) {
+    return arr[index] + addUpTo(arr, index - 1)
+  } else {
+    return arr[index]
+  }
+}
