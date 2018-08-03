75a00e724f1d7365b0e93df84655e32e04baac19
christphrd <diep.christopher@gmail.com>
2018-08-03 11:28:52 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 function printString(string) {
   console.log(string[0])
-
   if (string.length > 1) {
     printString(string.substring(1))
   } else {
@@ -20,7 +19,6 @@ function reverseString(str) {
 
 function isPalindrome(str) {
   if (str.length > 2 && str.slice(0,1) === str.slice(str.length - 1)) {
-    console.log(str.slice(1,str.length-1))
     return isPalindrome(str.slice(1,str.length-1))
   } else {
     return str.slice(0,1) === str.slice(str.length - 1)
@@ -36,23 +34,25 @@ function addUpTo(arr, index) {
 }
 
 function maxOf(arr) {
+  let copiedArr = arr.slice()
   if (arr.length > 1 && arr[0] > arr[arr.length -1]) {
-    arr.pop()
-    return maxOf(arr)
+    copiedArr.pop()
+    return maxOf(copiedArr)
   } else if (arr.length > 1 && arr[0] < arr[arr.length -1]) {
-    arr.shift()
-    return maxOf(arr)
+    copiedArr.shift()
+    return maxOf(copiedArr)
   } else {
     return arr[0]
   }
 }
 
 function includesNumber(arr, num) {
+  let copiedArr = arr.slice()
   if (arr[0] === num) {
     return true
   } else if (arr.length > 1) {
-    arr.shift()
-    return includesNumber(arr, num)
+    copiedArr.shift()
+    return includesNumber(copiedArr, num)
   } else {
     return arr[0] === num
   }
